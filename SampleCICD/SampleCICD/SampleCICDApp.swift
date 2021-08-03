8cf5e8f799eacc4c5caf9f2f8edbbabba10af766
dheerajRORO <74963441+dheerajRORO@users.noreply.github.com>
2021-08-03 20:05:50 +0530

    Add files via upload
    initial commit

---
+++ SampleCICD/SampleCICD/SampleCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCICDApp.swift
+//  SampleCICD
+//
+//  Created by Dheeraj Verma on 03/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
